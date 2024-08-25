370d7ab82733adb9956086c7f3d5340297476c31
grmkng <168589585+grmkng@users.noreply.github.com>
2024-08-25 21:29:03 +0100

    surface area difference

---
+++ surface area difference.js
@@ -0,0 +1,5 @@
+export default function calculateDifference(firstRectangle, secondRectangle) {
+    console.log("first rectangle", firstRectangle)
+    console.log("second rectangle", secondRectangle)
+    return (firstRectangle[0] * firstRectangle[1]) - (secondRectangle[0] * secondRectangle[1])
+}
